306a6985743c175cef86484dc10deed8e1ecac8e
crazysolutions25-tech <crazysolutions25@gmail.com>
2026-02-10 15:43:53 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: "/Siri-Tax-Consultancy/",  // <--- I ADDED IT HERE (Line 8)
       server: {
         port: 3000,
         host: '0.0.0.0',
